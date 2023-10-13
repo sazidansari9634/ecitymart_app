eebf1f62e454ed56f5e772cc1ddba1b4ad7026a2
Sazid Ansari <sazidansari9634@gmail.com>
2023-10-13 18:04:00 +0545

    home _controller setup with bottomnavvar setup

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -1,3 +1,4 @@
+
 const appname = "pMart";
 const appversion = "Version 1.0.0";
 const credits = "@Sazid Ans";
@@ -16,3 +17,6 @@ const loginWith = "Login in with";
 const privacyPolicy = "Privacy Policy";
 const termAndCond = "Terms and Conditions";
 const alreadyHaveAccount = "Already have an account? ";
+
+//home strings
+const home = "Home" , categories = "Categories", cart = "Cart", account = "Account";
